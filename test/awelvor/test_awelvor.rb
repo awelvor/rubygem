ccdfef85c6f16e6dc71b2b32bf2daf5cbd094966
awelvor <philippe.regent@gmail.com>
2024-10-25 15:15:20 +0200

    Update test_awelvor.rb

--- test/awelvor/test_awelvor.rb
+++ test/awelvor/test_awelvor.rb
@@ -1,1 +1,19 @@
+require "minitest/autorun"
+require "awelvor"
 
+class AwelvorTest < Minitest::Test
+  def test_english_hello
+    assert_equal "hello world",
+      Awelvor.hi("english")
+  end
+
+  def test_any_hello
+    assert_equal "hello world",
+      Awelvor.hi("ruby")
+  end
+
+  def test_spanish_hello
+    assert_equal "hola mundo",
+      Awelvor.hi("spanish")
+  end
+end
